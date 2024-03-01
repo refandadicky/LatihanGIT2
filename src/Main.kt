4f0f1ea8ce651d4d76bc8314f528a0324c35f932
refandadicky <refandadicky1@gmail.com>
2024-03-02 00:35:40 +0700

    Finishing Feature Circle

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,24 @@
-fun main() {
-    println("Hello World!")
+class App {
+
+    fun getAroundCircle(radius : Int){
+        val myResult = 2*3.14*radius
+        println("Around of circle is: $myResult")
+    }
+
+    fun getAreaCircle(radius : Int){
+        val myResult = 3.14 * radius *radius
+        println("Area of circle is: $myResult")
+    }
+    fun run(){
+        val radius = 7
+        getAreaCircle(radius)
+        getAroundCircle(radius)
+    }
+
+    companion object{
+        @JvmStatic
+        fun main(args: Array<String>) {
+            App().run()
+        }
+    }
 }
